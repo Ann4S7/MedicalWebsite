0664453212a01eb0a357c47da1f476b7141fd5b6
Ann4S7 <151684781+Ann4S7@users.noreply.github.com>
2024-02-08 21:19:19 +0100

    Appearance (#7)
    * improving the display of booked visits
    
    * small changes in text display
    
    * improving the design
    
    * simplifying the code
    
    * improving the base template

--- website/views.py
+++ website/views.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 from visits.models import Visit
 
 from datetime import datetime
@@ -15,17 +13,13 @@ def welcome(request):
 
     context = {}
     if request.user.is_active:
-        context["visits_patient"] = Visit.objects.filter(patient=request.user)
+        context["visits_patient_upcoming"] = Visit.objects.filter(past=False, patient=request.user)
 
     return render(request, "website/welcome.html", context)
-
-
-def about(request):
-    return HttpResponse("This is a website of our Clinic. You can book the services which we provide.")
 
 
 def out_of_date(visit):
     visit_datetime = datetime.combine(visit.date, visit.start_time)
     if visit_datetime < datetime.now():
-        visit.available = False
+        visit.past = True
         visit.save()
